e62d5056d6b0f6354a7789632d9b92c4577187df
Teramid <pszczotka1@gmail.com>
2023-02-26 19:26:37 +0100

    fiszki do podłączonej bazy

--- main.py
+++ main.py
@@ -54,6 +54,72 @@ def dwonload_verb_list():
 
 
 from os import listdir
+from os import system
+from random import randint
 
 files = listdir("baza")
 Files_Number = len(files)
+dict = {}
+for nr, x in enumerate(files):
+    file_name = "baza/" + x
+    f = open(file_name, "r")
+    f = f.readlines()
+    dict.update(
+        {nr: [f[0].rstrip(), f[1].rstrip(), f[2].rstrip(), f[3], file_name[5:]]}
+    )
+
+
+dictNumber = {}
+for i in range(1, Files_Number + 1):
+    dictNumber.update({i: 1})
+
+full_Answer = 0
+while True:
+    system("cls")
+    if full_Answer == Files_Number:
+        print("Po nauce czas na piwo :)")
+        break
+    else:
+        choose_File = int(randint(1, Files_Number))
+        if dictNumber.get(choose_File) == 0:
+            continue
+        else:
+            print("Ilosć pytań w bazie:", str(Files_Number))
+            print("Przyswojone pytania: ", full_Answer, "/", Files_Number)
+            print("Pytanie:", dict[choose_File][4])
+            print("pozostałych powtórzeń pytania:", dictNumber.get(choose_File))
+            print("")
+            print("Czasownik:", dict[choose_File][3])
+            Iform = input("I Forma: ")
+            if Iform == dict[choose_File][0]:
+                print("Correct")
+            else:
+                print("Uncorrect, should be: ", dict[choose_File][0])
+            IIform = input("II Forma: ")
+            if IIform == dict[choose_File][1]:
+                print("Correct")
+            else:
+                print("Uncorrect, should be: ", dict[choose_File][1])
+            IIIform = input("III Forma: ")
+            if IIIform == dict[choose_File][2]:
+                print("Correct")
+            else:
+                print("Uncorrect, should be: ", dict[choose_File][2])
+            x = dictNumber.get(choose_File)
+            try:
+                wait_press = input("Enter, aby kontynuować...")
+            except:
+                pass
+            if (
+                Iform == dict[choose_File][0]
+                and IIform == dict[choose_File][1]
+                and IIIform == dict[choose_File][2]
+            ):
+                x -= 1
+            else:
+                x += 2
+            dictNumber.update({choose_File: x})
+            if x == 0:
+                full_Answer += 1
+            else:
+                pass
