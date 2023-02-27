73fc0dd04e3d281f8acc5119a0f9cd4efde27f77
Teramid <81380951+Teramid@users.noreply.github.com>
2023-02-27 10:54:47 +0100

    Sprawdzanie czy istnieje baza i pobieranie jeśli nie

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
+from random import randint
+
+
 def download_verb_list():
     from urllib.request import urlopen
     from bs4 import BeautifulSoup
 
     """_summary_
-    Aplikacja do tworzenia bazy danych do nauki angielskiego
+    Funkcja tworząca baze czasowników nieregularnych
     """
 
     html = "https://www.ang.pl/gramatyka/czasowniki-verbs/czasowniki-nieregularne"
@@ -31,12 +35,6 @@ def download_verb_list():
                 [word_list[nr - 3], word_list[nr - 2], word_list[nr - 1], word_list[nr]]
             )
 
-    for verb in verb_irr:
-        print(verb)
-
-    print(len(verb_irr))
-
-    f = open("baza.txt", "w")
     for nr, verb in enumerate(verb_irr):
         nameFile = ""
         numberFile = nr + 1
@@ -47,17 +45,25 @@ def download_verb_list():
         else:
             nameFile = str(numberFile) + ".txt"
         nameFile = "baza/" + nameFile
+        if not os.path.exists("baza"):
+            os.makedirs("baza")
+        else:
+            pass
         f = open(nameFile, "w")
         f.write("\n".join(x for x in verb))
-
+    print("Pobrano", len(verb_irr), "czasowników")
+    Enter_press = input("Wciśnij enter aby kontynuować")
     f.close()
 
 
-from os import listdir
-from os import system
-from random import randint
+if os.path.exists("baza") and os.path.isdir("baza"):
+    pass
+else:
+    print("Baza pytań nie istnieje\nJeśli chcesz pobrać wciśnij Enter")
+    enter_press = input()
+    download_verb_list()
 
-files = listdir("baza")
+files = os.listdir("baza")
 Files_Number = len(files)
 dict = {}
 for nr, x in enumerate(files):
@@ -65,20 +71,26 @@ for nr, x in enumerate(files):
     f = open(file_name, "r")
     f = f.readlines()
     dict.update(
-        {nr: [f[0].rstrip(), f[1].rstrip(), f[2].rstrip(), f[3], file_name[5:]]}
+        {nr + 1: [f[0].rstrip(), f[1].rstrip(), f[2].rstrip(), f[3], file_name[5:]]}
     )
 
 
 dictNumber = {}
 for i in range(1, Files_Number + 1):
     dictNumber.update({i: 1})
-
+form_name = [
+    "Bezokolicznik (infinitive): ",
+    "II forma (past tense): ",
+    "III forma (past participle): ",
+]
 full_Answer = 0
 while True:
-    system("cls")
+    os.system("cls")
     if full_Answer == Files_Number:
         print("Po nauce czas na piwo :)")
+        wait_press = input("Enter, aby wyjść...")
         break
+
     else:
         choose_File = int(randint(1, Files_Number))
         if dictNumber.get(choose_File) == 0:
@@ -86,38 +98,26 @@ while True:
         else:
             print("Ilosć pytań w bazie:", str(Files_Number))
             print("Przyswojone pytania: ", full_Answer, "/", Files_Number)
+
             print("Pytanie:", dict[choose_File][4])
             print("pozostałych powtórzeń pytania:", dictNumber.get(choose_File))
             print("")
             print("Czasownik:", dict[choose_File][3])
-            Iform = input("I Forma: ")
-            if Iform == dict[choose_File][0]:
-                print("Correct")
-            else:
-                print("Uncorrect, should be: ", dict[choose_File][0])
-            IIform = input("II Forma: ")
-            if IIform == dict[choose_File][1]:
-                print("Correct")
-            else:
-                print("Uncorrect, should be: ", dict[choose_File][1])
-            IIIform = input("III Forma: ")
-            if IIIform == dict[choose_File][2]:
-                print("Correct")
-            else:
-                print("Uncorrect, should be: ", dict[choose_File][2])
+            correct_Answer = 0
+            # print(len(dict[choose_File]))
+            # print(dict[choose_File])
+            for i in range(len(dict[choose_File]) - 2):
+                if input(form_name[i]) in dict[choose_File][i].split():
+                    print("Correct")
+                    correct_Answer += 1
+                else:
+                    print("Uncorrect, should be: ", dict[choose_File][i])
+            wait_press = input("\nEnter, aby przejść do następnego...")
             x = dictNumber.get(choose_File)
-            try:
-                wait_press = input("Enter, aby kontynuować...")
-            except:
-                pass
-            if (
-                Iform == dict[choose_File][0]
-                and IIform == dict[choose_File][1]
-                and IIIform == dict[choose_File][2]
-            ):
+            if correct_Answer >= 3:
                 x -= 1
             else:
-                x += 2
+                x -= 1
             dictNumber.update({choose_File: x})
             if x == 0:
                 full_Answer += 1
